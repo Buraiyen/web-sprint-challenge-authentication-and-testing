fc86de4d72ab6f8f871b4ba3a8da420ebe982881
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-12-31 16:38:20 -0800

    test: validate missing credentials

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,21 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+const Users = require('./auth/auth-model');
+const request = require('supertest');
+const server = require('../api/server');
+const db = require('../data/dbConfig');
+
+describe('POST /api/auth/register', () => {
+  beforeEach(async () => {
+    await db('users').where({ id: 2 }).del();
+  });
+  it('throws an error with missing credentials', async () => {
+    const user = { username: '', password: '32fe' };
+    // await Users.insert(user);
+    const response = await request(server)
+      .post('/api/auth/register')
+      .send(user);
+    const responseText = JSON.parse(response.text).message;
+    expect(response.status).toEqual(404);
+    expect(responseText).toEqual('username and password required');
+  });
+});
