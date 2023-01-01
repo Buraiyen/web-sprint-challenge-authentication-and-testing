081acdf1c443ebe0d61a85a870fcb9d93dab854f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-01 12:11:16 -0800

    installed express session

--- api/server.js
+++ api/server.js
@@ -8,7 +8,21 @@ const authRouter = require('./auth/auth-router.js');
 const jokesRouter = require('./jokes/jokes-router.js');
 
 const server = express();
+const session = require('express-session');
 
+const sessionConfig = {
+  name: 'cookie', // default would the the cookie id
+  secret: 'keep it secret', // should be saved in an environment variable
+  cookie: {
+    maxAge: 1000 * 30, // valid for 30 seconds
+    secure: false, // should be true in production
+    httpOnly: true, // cannot be accessed from javascript
+  },
+  resave: false, // 'do we want to recreate a session even if it hasne't changed?'
+  saveUnintialized: false, // GDPR laws agaisnst setting cookies automatically. User should opt in
+};
+
+server.use(session(sessionConfig));
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
