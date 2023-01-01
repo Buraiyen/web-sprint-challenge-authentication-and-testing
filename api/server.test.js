28eb67543b8d8f4d2162c8cb90cb9ea3c1b96c92
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-01-01 10:54:33 -0800

    test: reformatted suites, added 750ms timeout

--- api/server.test.js
+++ api/server.test.js
@@ -15,53 +15,55 @@ beforeEach(async () => {
   await db.migrate.latest();
 });
 
-describe('POST /api/auth/register', () => {
-  const REGISTER_URL = '/api/auth/register';
+describe('User registration and authentication', () => {
+  describe('POST /api/auth/register', () => {
+    const REGISTER_URL = '/api/auth/register';
 
-  it('should insert the user into the database', async () => {
-    const user = { username: 'Kitboga', password: 'abcd' };
-    await appTest.post(REGISTER_URL).send(user);
-    const users = await Users.getAll();
-    expect(users).toHaveLength(1);
-  }, 2000);
+    it('should insert the user into the database', async () => {
+      const user = { username: 'Kitboga', password: 'abcd' };
+      await appTest.post(REGISTER_URL).send(user);
+      const users = await Users.getAll();
+      expect(users).toHaveLength(1);
+    }, 2000);
 
-  it(`should have 'id', 'username', and 'password' in response body`, async () => {
-    const user = { username: 'Nairb', password: 'abcd' };
-    const response = await appTest.post(REGISTER_URL).send(user);
-    const responseBody = JSON.parse(response.text);
-    let hasFields = false;
-    if (
-      'id' in responseBody &&
-      'username' in responseBody &&
-      'password' in responseBody
-    ) {
-      hasFields = true;
-    }
-    expect(hasFields).toBe(true);
-  });
+    it(`should have 'id', 'username', and 'password' in response body`, async () => {
+      const user = { username: 'Nairb', password: 'abcd' };
+      const response = await appTest.post(REGISTER_URL).send(user);
+      const responseBody = JSON.parse(response.text);
+      let hasFields = false;
+      if (
+        'id' in responseBody &&
+        'username' in responseBody &&
+        'password' in responseBody
+      ) {
+        hasFields = true;
+      }
+      expect(hasFields).toBe(true);
+    }, 750);
 
-  it('should hash the password', async () => {
-    const user = { username: 'Alfred', password: '32fe' };
-    const response = await appTest.post(REGISTER_URL).send(user);
-    const responsePassword = JSON.parse(response.text).password;
-    expect(user.password).not.toEqual(responsePassword);
-    expect(responsePassword.length).toEqual(60);
-  });
+    it('should hash the password', async () => {
+      const user = { username: 'Alfred', password: '32fe' };
+      const response = await appTest.post(REGISTER_URL).send(user);
+      const responsePassword = JSON.parse(response.text).password;
+      expect(user.password).not.toEqual(responsePassword);
+      expect(responsePassword.length).toEqual(60);
+    }, 750);
 
-  it('throws an error if payload has missing credentials', async () => {
-    const user = { username: '', password: '32fe' };
-    const response = await appTest.post(REGISTER_URL).send(user);
-    const responseText = JSON.parse(response.text).message;
-    expect(response.status).toEqual(404);
-    expect(responseText).toEqual('username and password required');
-  });
+    it('throws an error if payload has missing credentials', async () => {
+      const user = { username: '', password: '32fe' };
+      const response = await appTest.post(REGISTER_URL).send(user);
+      const responseText = JSON.parse(response.text).message;
+      expect(response.status).toEqual(404);
+      expect(responseText).toEqual('username and password required');
+    }, 750);
 
-  it('throws an error if username exists', async () => {
-    const user = { username: 'Banri', password: '32fe' };
-    await request(server).post(REGISTER_URL).send(user);
-    const response = await appTest.post(REGISTER_URL).send(user);
-    const responseText = JSON.parse(response.text).message;
-    expect(response.status).toEqual(404);
-    expect(responseText).toEqual('username exists');
+    it('throws an error if username exists', async () => {
+      const user = { username: 'Banri', password: '32fe' };
+      await request(server).post(REGISTER_URL).send(user);
+      const response = await appTest.post(REGISTER_URL).send(user);
+      const responseText = JSON.parse(response.text).message;
+      expect(response.status).toEqual(404);
+      expect(responseText).toEqual('username taken');
+    }, 750);
   });
 });
